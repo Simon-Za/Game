50f0ec8a3519399d499ca1d90bd3a95cbd0cf413
Simon <simon.felix.zakowski@hs-furtwangen.de>
2020-11-06 18:06:31 +0100

    added Wiki Folder and went further towards lane runner

--- Starship/Starship.cs
+++ Starship/Starship.cs
@@ -15,6 +15,9 @@ using System.Linq;
 using System.ComponentModel;
 using Fusee.Xirkit;
 using System.Text;
+using System.Security.Cryptography;
+using OpenTK.Graphics.OpenGL;
+using System.Threading;
 
 namespace FuseeApp
 {
@@ -27,9 +30,10 @@ namespace FuseeApp
 
 
         private Transform _starshipTrans;
-        private Transform _trenchTrans;
 
         private Mesh _starShipMesh;
+
+        private SceneNode TrenchParent;
 
 
    
@@ -47,6 +51,10 @@ namespace FuseeApp
         bool down;
 
 
+        float counter;//1sec
+
+        float3 oldPos;
+        float3 newPos;
 
         //Kollisionsvariablen 
         private AABBf _shipBox; 
@@ -54,149 +62,129 @@ namespace FuseeApp
         private Transform _cubeObstTrans;
       
         private Mesh _cubeObstMesh;
-      
 
 
 
+        List<SceneNode> TrenchesList;
 
 
         public override void Init()
         {
             RC.ClearColor = new float4(0, 0, 0, 0);
 
-            _starshipScene = AssetStorage.Get<SceneContainer>("StarshipProtoSeparateTrenches.fus");
+            _starshipScene = AssetStorage.Get<SceneContainer>("StarshipProto.fus");
 
-            //Trench0; 1, 2, 3
+
+            _starshipTrans = _starshipScene.Children.FindNodes(node => node.Name == "Ship")?.FirstOrDefault()?.GetTransform();
+
+            _starShipMesh = _starshipScene.Children.FindNodes(node => node.Name == "Ship")?.FirstOrDefault()?.GetMesh();
+
+            oldPos = _starshipTrans.Translation;
+            newPos = _starshipTrans.Translation;
+
+
+            TrenchesList = new List<SceneNode>
+            {
+                AssetStorage.Get<SceneContainer>("Trench 1.1.fus").ToSceneNode(),
+                AssetStorage.Get<SceneContainer>("Trench 2.fus").ToSceneNode(),
+                AssetStorage.Get<SceneContainer>("Trench 3.fus").ToSceneNode()
+            };
+
+
+            int n = TrenchesList.Count();
+            Random random = new Random();
+
+
+
+            var currentTrench = TrenchesList[0];
+            var newTrench = TrenchesList[random.Next(0, n - 1)];
+
+            TrenchParent = new SceneNode()
+            {
+                Name = "TrenchParent"
+            };
+
+            TrenchParent.Children.Add(currentTrench);
+
+
+            _starshipScene.Children.Add(TrenchParent);
+
 
             _sceneRenderer = new SceneRendererForward(_starshipScene);
 
         }
 
-
+       
 
         public override void RenderAFrame()
         {
+
+
+
             // Clear the backbuffer
-           RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             RC.Viewport(0, 0, Width, Height);
-
-
-    
-            _starshipTrans = _starshipScene.Children.FindNodes(node => node.Name == "Ship")?.FirstOrDefault()?.GetTransform();
-
-            _starShipMesh = _starshipScene.Children.FindNodes(node => node.Name == "Ship")?.FirstOrDefault()?.GetMesh();
-
-            //_trenchTrans = _starshipScene.Children.FindNodes(node => node.Name == "Trench1")?.FirstOrDefault()?.GetTransform();
-
-
 
 
 
             double speed = (double)DeltaTime * 20;
 
 
-
-            //Bounding Boxes 
-
-            //Boundind Box des Schiffs
-            _shipBox = _starShipMesh.BoundingBox;
-          
-
-
-            //Bounding Box des Cube Obstacles
-            //_obstBox = _cubeObstMesh.BoundingBox;
-
-
-
-            //Hier werden für Trenches sowie ihre jeweiligen obstacles Listen erstellt, die einzeln abgegangen werden, um nach einer Kollision zu prüfen
-            List<SceneNode> TrenchList = _starshipScene.Children.FindNodes(node => node.Name.Contains("Trench")).ToList();
-            TrenchList.RemoveAt(1);
-            TrenchList.RemoveAt(1);
-
-            if (TrenchList.Count() > 1)
-            {
-                TrenchList.RemoveAt(2);
-                TrenchList.RemoveAt(2);
-            }
-
-
-
-            //System.Console.WriteLine(TrenchList.Count);
-
-            for (int j = 0; j < TrenchList.Count(); j++)
-            { 
-                _trenchTrans = TrenchList.ElementAt(j).GetTransform();
-                //System.Console.WriteLine(_trenchTrans.Translation);
-
-                //System.Console.WriteLine(_trenchTrans);
-
-                if (_trenchTrans != null)
-                {
-                    List<SceneNode> ObstaclesList = TrenchList.ElementAt(j).Children.FindNodes(node => node.Name.Contains("ObstacleDummy")).ToList();
-
-                    for (int i = 0; i < ObstaclesList.Count(); i++)
-                    {
-                        _cubeObstTrans = _starshipScene.Children.FindNodes(node => node.Name == "ObstacleDummy" + i)?.FirstOrDefault()?.GetTransform();
-
-                        _cubeObstMesh = _starshipScene.Children.FindNodes(node => node.Name == "ObstacleDummy" + i)?.FirstOrDefault()?.GetMesh();
-
-                        AABBf cubeHitbox = _trenchTrans.Matrix() * _cubeObstTrans.Matrix() * _cubeObstMesh.BoundingBox;
-
-                        Trench(_shipBox, cubeHitbox, start, speed, playTime);
-                    }
-                }
-                
-            }
-
-
-            //System.Console.WriteLine(start);
-
-
+ 
 
             //Steuerung links/rechts
             if (Keyboard.IsKeyDown(KeyCodes.Left))
             {
                 if (left)
                 {
-                    //do nothing
                     mid = false;
                 }
                 else if (mid)
                 {
-                    _starshipTrans.Translation.x -= 3;
+                    oldPos = newPos;
+                    newPos.x = -3;
+                    counter = 0.3f;
+
                     mid = false;
                     left = true;
                     
                 }
                 else if (right)
                 {
-                    _starshipTrans.Translation.x -= 3;
+                    oldPos = newPos;
+                    newPos.x = 0;
+                    counter = 0.3f;
+
                     right = false;
                     mid = true;
                 }
-                _starshipTrans.Rotation.x = Keyboard.LeftRightAxis * 0.167f;
+
             }
             if (Keyboard.IsKeyDown(KeyCodes.Right))
             {
                 if (left)
                 {
-                    _starshipTrans.Translation.x += 3;
+                    oldPos = newPos;
+                    newPos.x = 0;
+                    counter = 0.3f;
+
                     left = false;
                     mid = true;
                 }
                 else if (mid)
                 {
-                    _starshipTrans.Translation.x += 3;
+                    oldPos = newPos;
+                    newPos.x = 3;
+                    counter = 0.3f;
+
                     mid = false;
                     right = true;
                 }
                 else if (right)
                 {
-                    //do nothing
                     mid = false;
                 }
-                _starshipTrans.Rotation.x = Keyboard.LeftRightAxis * 0.167f;
             }
 
             //oben / unten
@@ -247,15 +235,13 @@ namespace FuseeApp
 
 
 
-
-
             //flüssige Steuerung
 
             //if (Keyboard.LeftRightAxis != 0)
-            //{               
+            //{
             //    if (Keyboard.LeftRightAxis > 0) //rechts
             //    {
-            //         _starshipTrans.Translation.x += 10 * DeltaTime * Keyboard.LeftRightAxis;
+            //        _starshipTrans.Translation.x += 10 * DeltaTime * Keyboard.LeftRightAxis;
 
             //        //leichter tilt nach links
             //        _starshipTrans.Rotation.x = Keyboard.LeftRightAxis * 0.167f;
@@ -285,7 +271,7 @@ namespace FuseeApp
             //    //Console.WriteLine(_starshipTrans.Translation.x);
             //}
 
-            //Steuerung oben/unten
+            ////Steuerung oben/ unten
 
             //if (Keyboard.UpDownAxis != 0)
             //{
@@ -314,7 +300,7 @@ namespace FuseeApp
             //        //Bewegungsgrenze unten
             //        if (_starshipTrans.Translation.y <= -0.3f)
             //        {
-            //        _starshipTrans.Translation.y -= 7 * DeltaTime * Keyboard.UpDownAxis;
+            //            _starshipTrans.Translation.y -= 7 * DeltaTime * Keyboard.UpDownAxis;
             //        }
             //    }
             //}
@@ -326,31 +312,85 @@ namespace FuseeApp
             //Hier Startbutton / Menü einfügen
 
 
-           
+
 
             if (Keyboard.IsKeyDown(KeyCodes.Space))  //wenn die Leertaste gedrückt wird, wird die Zeit seit dem Drücken in playTime gespeichert
             {
                 start = true;
                 appStartTime = RealTimeSinceStart;
-                var irgendwas = AssetStorage.Get<SceneContainer>("Trench 1.fus").ToSceneNode();
-                    _starshipScene.Children.Add(irgendwas);
+
+                //var irgendwas = AssetStorage.Get<SceneContainer>("Trench 1.fus").ToSceneNode();
+                   // _starshipScene.Children.Add(irgendwas);
             }
 
 
-            if(start)
+            //Bounding Boxes 
+
+            //Boundind Box des Schiffs
+            _shipBox = _starShipMesh.BoundingBox;
+
+
+
+
+            if (start)
             { 
                 //speed *= playTime;            unnötig, aber in ähnlicher Form später für die Geschwindigkeitserhöhung nutzbar
-                _trenchTrans.Translation.z -= (float)speed;         //Die Bewegung der Szene wird aktiviert
+               
                 playTime = StartTimer(appStartTime);
-                
+
+                //Hier werden für Trenches sowie ihre jeweiligen obstacles Listen erstellt, die einzeln abgegangen werden, um nach einer Kollision zu prüfen
+                for (int j = 0; j < TrenchParent.Children.Count; j++)
+                {
+                    var _trenchTrans = TrenchParent.Children.ElementAt(j).GetTransform();
+                    _trenchTrans.Translation.z -= (float)speed;         //Die Bewegung der Szene wird aktiviert
+                    //System.Console.WriteLine(_trenchTrans.Translation);
+
+                    //System.Console.WriteLine(_trenchTrans.Translation);
+
+                    if (_trenchTrans != null)
+                    {
+                        List<SceneNode> ObstaclesList = TrenchParent.Children.ElementAt(j).Children.FindNodes(node => node.Name.Contains("ObstacleDummy")).ToList();
+
+                        for (int i = 0; i < ObstaclesList.Count(); i++)
+                        {
+                            _cubeObstTrans = TrenchParent.Children.ElementAt(j).Children.FindNodes(node => node.Name == "ObstacleDummy" + i)?.FirstOrDefault()?.GetTransform();
+
+                            _cubeObstMesh = TrenchParent.Children.ElementAt(j).Children.FindNodes(node => node.Name == "ObstacleDummy" + i)?.FirstOrDefault()?.GetMesh();
+
+                            AABBf cubeHitbox = _trenchTrans.Matrix() * _cubeObstTrans.Matrix() * _cubeObstMesh.BoundingBox;
+                            
+
+                            Trench(_shipBox, cubeHitbox);
+                        }
+                    }
+                }
             }
             else
             {
                 playTime = 0;  //???? idk ob das so was bringt, aber ist erstmal egal
                 speed = 0;
             }
-      
 
+
+            if (counter > 0)
+            {
+                _starshipTrans.Translation = float3.Lerp(newPos, oldPos, M.SmootherStep((counter)/0.3f));
+               if (newPos.x < oldPos.x)
+                {
+                    _starshipTrans.Rotation.x = M.SmootherStep(counter / 0.3f) * -0.167f;
+                }
+               else if (newPos.x > oldPos.x)
+                {
+                    _starshipTrans.Rotation.x = M.SmootherStep(counter/0.3f) * 0.167f;
+                }
+                
+                counter -= DeltaTime;
+            }
+            else if (counter < 0)
+            {
+                counter = 0;
+                _starshipTrans.Rotation.x = 0;
+            }
 
 
             RC.View =  float4x4.LookAt(0, 3, -8, 0, 2, 0, 0, 1, 0);   
@@ -363,8 +403,6 @@ namespace FuseeApp
             Present();
 
         }
-
-
 
 
 
@@ -381,8 +419,7 @@ namespace FuseeApp
     
         
         
-        //Trench Class
-        private void Trench(AABBf shipHitbox, AABBf cubeHitbox,bool start, double speed, float playTime)
+        private void Trench(AABBf shipHitbox, AABBf cubeHitbox)
         {
    
             if (shipHitbox.Intersects(cubeHitbox))
@@ -391,8 +428,6 @@ namespace FuseeApp
                 Death();
             }  
         }
-
-
 
 
         private void Death()
@@ -416,115 +451,5 @@ namespace FuseeApp
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //// Horizontal and vertical rotation Angles for the displayed object 
-        //private static float _angleHorz = M.PiOver4, _angleVert;
-
-        //// Horizontal and vertical angular speed
-        //private static float _angleVelHorz, _angleVelVert;
-
-        //// Overall speed factor. Change this to adjust how fast the rotation reacts to input
-        //private const float RotationSpeed = 7;
-
-        //// Damping factor 
-        //private const float Damping = 0.8f;
-
-        //private SceneContainer _rocketScene;
-        //private SceneRendererForward _sceneRenderer;
-
-        //private bool _keys;
-
-        //// Init is called on startup. 
-        //public override void Init()
-        //{
-        //    // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
-        //    RC.ClearColor = new float4(0, 0, 0, 0);
-
-        //    // Load the rocket model
-        //    _rocketScene = AssetStorage.Get<SceneContainer>("StarshipProtoSmall.fus");
-
-        //    // Wrap a SceneRenderer around the model.
-        //    _sceneRenderer = new SceneRendererForward(_rocketScene);
-        //}
-
-        //// RenderAFrame is called once a frame
-        //public override void RenderAFrame()
-        //{
-        //    // Clear the backbuffer
-        //    RC.Clear(ClearFlags.Color | ClearFlags.Depth);
-
-        //    RC.Viewport(0, 0, Width, Height);
-
-        //    // Mouse and keyboard movement
-        //    if (Keyboard.LeftRightAxis != 0 || Keyboard.UpDownAxis != 0)
-        //    {
-        //        _keys = true;
-        //    }
-
-        //    if (Mouse.LeftButton)
-        //    {
-        //        _keys = false;
-        //        _angleVelHorz = -RotationSpeed * Mouse.XVel * DeltaTime * 0.0005f;
-        //        _angleVelVert = -RotationSpeed * Mouse.YVel * DeltaTime * 0.0005f;
-        //    }
-        //    else if (Touch.GetTouchActive(TouchPoints.Touchpoint_0))
-        //    {
-        //        _keys = false;
-        //        var touchVel = Touch.GetVelocity(TouchPoints.Touchpoint_0);
-        //        _angleVelHorz = -RotationSpeed * touchVel.x * DeltaTime * 0.0005f;
-        //        _angleVelVert = -RotationSpeed * touchVel.y * DeltaTime * 0.0005f;
-        //    }
-        //    else
-        //    {
-        //        if (_keys)
-        //        {
-        //            _angleVelHorz = -RotationSpeed * Keyboard.LeftRightAxis * DeltaTime;
-        //            _angleVelVert = -RotationSpeed * Keyboard.UpDownAxis * DeltaTime;
-        //        }
-        //        else
-        //        {
-        //            var curDamp = (float)System.Math.Exp(-Damping * DeltaTime);
-        //            _angleVelHorz *= curDamp;
-        //            _angleVelVert *= curDamp;
-        //        }
-        //    }
-
-        //    _angleHorz += _angleVelHorz;
-        //    _angleVert += _angleVelVert;
-
-        //    // Create the camera matrix and set it as the current View transformation
-        //    var mtxRot = float4x4.CreateRotationX(_angleVert) * float4x4.CreateRotationY(_angleHorz);
-        //    var mtxCam = float4x4.LookAt(0, 3, -8, 0, 2, 0, 0, 1, 0);
-        //    RC.View = mtxCam * mtxRot;
-
-        //    // Tick any animations and Render the scene loaded in Init()
-        //    _sceneRenderer.Render(RC);
-
-        //    // Swap buffers: Show the contents of the backbuffer (containing the currently rendered frame) on the front buffer.
-        //    Present();
-        //}
     }
 }
---
+++ Wiki/AABBf.md
@@ -0,0 +1,80 @@
+AABBf 
+
+Work in progress
+
+The Axis-Aligned Bounding Box is an efficient tool for collision detection.
+A Bounding Box forms itself out of the minimal and the maximal x, y, z coordinates of an object.
+The min and max values of a cube in the center of a scene may look like this:
+
+    min = (-1, -1, -1); 
+    max = (1, 1, 1);
+
+A Bounding Box draws a cuboid as tightly as possible around the targeted object.
+A sphere with radius 1 would have the same Bounding Box as the cube in the example above.
+
+![minimum and maximum values visualized](formingAABB.jpeg)
+
+The Axis-Aligned Bounding Box is as the name suggests "axis-aligned", which means the Bounding Boxes are not able to rotate and if its object rotates the AABB has to be recalculated and may end up in a bad approximation of its former shape.
+Therefore the AABB is most useful when the objects principal axes are roughly aligned with the scenes coordinate system.
+
+![rotating an object in a secen visualized](rotatingObject.jpeg)
+
+When two Bounding Boxes are tested for a collision, the minimum values of one box are compared to the maximum of the other and vice versa (A.min.y & B.max.y,..., A.max.z & B.min.z,...). So if a minimum value is greater than a maximum value / a maximum value is lesser than a minimum value, a collision occurs.
+
+
+Components ###
+
+| Type      | Name                             |        Description                                                             |
+| ----------|:--------------------------------:| ------------------------------------------------------------------------------:|
+| 'AABBf'   | AABBf()                          | Creates a new axis aligned bounding box                                        |
+| 'bool'    | Equals(B)                        | Indicates whether this plane is equal to another object                        |
+| 'bool'    | Intersects(B)                    | Checks if this AABB intersects with another                                    |
+| 'AABBf'   | ReferenceEquals(A, C)            | Determines whether the specified object instances are the same isntance        |
+| 'AABBf'   | Union(A, B)                      | Calculates the bounding box around two exosting bounding boxes                 |
+| 'int'     | GetHashCode()                    | Returns the hash code for this instance                                        |
+| 'Type'    | GetType()                        | Gets the Type of the current instance                                          |
+| 'bool'    | InsideOrIntersectingFrustrum()   | Checks if a viewing frustrum lies within or intersects this AABB               |
+| 'bool'    | InsideOrIntersectingPlane()      | Checks if a viewing plane lies within or intersects this AABB                  |
+| 'string'  | ToString()                       | Returns the fully qualified type name of this instance                         |
+| 'float3'  | Size                             | Returns the width, height and depth of the box in x, y and z                   |
+| 'float3'  | Center                           | Returns the center of the bounding box                                         |
+| 'float3'  | min                              | The minimum value of the axis aligned bounding box in x, y and z direction     |
+| 'float3'  | max                              | The maximum value of the axis aligned bounding box in x, y and z direction     |
+
+
+Building your own Bounding Box ###
+
+You can use the constructor 'AABBf' to build your own Bounding Box: #####
+
+
+'_min = new float3(-1, -1, -1);
+_max = new float3(1, 1, 1):
+
+AABBf bb1 = new AABBf
+(
+    _min,
+    _max 
+);'
+
+
+There is also a way to get the Bounding Box of an Asset or Object in the scene: #####
+
+
+'private Transform SphereTrans;
+private Mesh SphereMesh;
+
+SphereMesh = Scene.Children.FindNodes(node =>node.Name == "Sphere")?.FirstOrDefault()?.GetMesh();
+bb2 = SphereMesh.BoundingBox;'
+
+
+If (at least) one of your object moves you can multiplicate it with the Matrix of the moving Object: #####
+
+'AABBf movingBb = bb2 * SphereTrans.Matrix();'
+
+
+Now if you want to check for a collision between these two Bounding Boxes, you can use the Intersect Method: #####
+
+'if(bb1.Intersects(bb2))
+{
+    Console.WriteLine(Oh? You're intersecting me?);
+}'
